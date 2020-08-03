38c1b2e28019eb89f9391e61907b720c1e942e66
Robert Carter <racarter1215@gmail.com>
2020-08-03 10:47:34 -0700

    Robert Push 2: Made additions to DbContext

--- HobbyListForHobbyist/Data/HobbyListDbContext.cs
+++ HobbyListForHobbyist/Data/HobbyListDbContext.cs
@@ -20,13 +20,70 @@ namespace HobbyListForHobbyist.Data
             base.OnModelCreating(modelBuilder);
 
             // ============= TODO declare JoinTable's compositeKeys here ============================
-            // Example: modelBuilder.Entity<MiniToPaint().HasKey(x => new { x.MiniModelId, x.PaintId });
-            
+            modelBuilder.Entity<MiniToPaint>().HasKey(x => new { x.MiniModelId, x.PaintId });
             // ======== TODO seed the database with info here ======================
-            // Example: modelBuilder.Entity<MiniModel>().HasData(new MiniModel {...});
+            modelBuilder.Entity<MiniModel>().HasData(
+                 new MiniModel
+                {
+                    Id = 1,
+                    Name = "Platoon Leader",
+                    Manufacturer = "Forge Fire",
+                    PartNumber = "111",
+                    Faction = "North Army",
+                    PointCost = 100,
+                    BuildState = BuildState.built
+                },
+                 new MiniModel
+                 {
+                    Id = 2,
+                    Name = "Heavy Support Squad",
+                    Manufacturer = "Forge Fire",
+                    PartNumber = "122",
+                    Faction = "South Army",
+                    PointCost = 75,
+                    BuildState = BuildState.painted
+                },
+                  new MiniModel
+                  {
+                      Id = 3,
+                      Name = "Chariot Armored Personnel Carrier",
+                      Manufacturer = "Forge Fire",
+                      PartNumber = "122",
+                      Faction = "East Army",
+                      PointCost = 250,
+                      BuildState = BuildState.unBuilt
+                  });
+            modelBuilder.Entity<Paint>().HasData(
+                new Paint 
+                {
+                    Id = 1,
+                    ColorName = "Gunmetal",
+                    Manufacturer = "Forge Fire",
+                    ProductNumber = "32"
+                },
+                new Paint
+                {
+                    Id = 2,
+                    ColorName = "Patriot Green",
+                    Manufacturer = "Forge Fire",
+                    ProductNumber = "44"
+                },
+                new Paint
+                {
+                    Id = 3,
+                    ColorName = "Alabaster",
+                    Manufacturer = "Forge Fire",
+                    ProductNumber = "78"
+                }
+                );
+
         }
 
         // ================ TODO Connect the DB with the model classes. 
-        // example:  DbSet<MiniModel> MiniModels { get; set; }
+        public DbSet<MiniModel> MiniModels { get; set; }
+        public DbSet<MiniToPaint> MinisToPaint { get; set; }
+        public DbSet<MiniToSupply> MinisToSupply { get; set; }
+        public DbSet<Paint> Paints { get; set; }
+
     }
 }
--- HobbyListForHobbyist/Models/MiniModel.cs
+++ HobbyListForHobbyist/Models/MiniModel.cs
@@ -8,20 +8,22 @@ namespace HobbyListForHobbyist.Models
     public class MiniModel
     {
         //Properties
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Manufacturer { get; set; }
         public string PartNumber { get; set; }
         public string Faction { get; set; }
         public int PointCost { get; set; }
+        public BuildState BuildState { get; set; }
         public string UserId { get; set; }
 
         // Nav Properties
-        public List<MinisToSupplies> MinisToSupplies { get; set; }
+        public List<MiniToSupply> MinisToSupplies { get; set; }
         public List<MiniToPaint> MinisToPaints { get; set; }
     }
     }
 
-    public enum BuidState
+    public enum BuildState
     {
         unBuilt = 0,
         built,
