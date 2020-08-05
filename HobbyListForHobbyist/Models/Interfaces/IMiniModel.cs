09dc83f4b9242fd59fb33f251b546de7535a1e64
Robert Carter <racarter1215@gmail.com>
2020-08-05 15:14:49 -0700

    minimodel and supply are commented

--- HobbyListForHobbyist/Models/Interfaces/IMiniModel.cs
+++ HobbyListForHobbyist/Models/Interfaces/IMiniModel.cs
@@ -9,16 +9,45 @@ namespace HobbyListForHobbyist.Models.Interfaces
     public interface IMiniModel
     {
         // CreateAMiniModel
+        /// <summary>
+        /// The below method allows one to create an minimodel to be stored within MiniModel
+        /// </summary>
+        /// <param name="supplyName">the name of the minimodel item</param>
+        /// <returns>the newly added minimodel item</returns>
         Task<MiniModelDTO> Create(MiniModelDTO miniModel, string email);
-        // GetAllMiniModels
+        // GetAMiniModel
+        /// <summary>
+        /// the below method allows one to seach for a particular minimodel item
+        /// </summary>
+        /// <param name="email">this is the email associated with the current user (ensures they have access to minimodel item)</param>
+        /// <returns>the minimodel item in question</returns>
         Task<MiniModelDTO> GetMiniModel(int id, string email);
         // GetAllMiniModelsOfState
         Task<List<MiniModelDTO>> GetAMiniModelOfState(BuildState buildState, string email);
-        // GetAMiniModel
+        // GetAllMiniModels
+        /// <summary>
+        /// the below method brings up to the user all minimodels items currently in their table
+        /// </summary>
+        /// <param name="supplyId">the id number of each particular minimodel item (the method checks for them all)</param>
+        /// <param name="email">authenticates the user's access level for the minimodel items</param>
+        /// <returns>the minimodels items in the database</returns>
         Task<List<MiniModelDTO>> GetAllMiniModels(string email);
         // UpdateAMiniModel
+        // UpdateAMiniModel
+        /// <summary>
+        /// the below method updates a particular minimodel item in the database
+        /// </summary>
+        /// <param name="supplyDTO">this selects the minimodel DTO (what parameters are shown to user)</param>
+        /// <param name="email">checks to see if user has authorization for selected minimodel item</param>
+        /// <returns>the minimodel dto with the updated minimodel item</returns>
         Task<MiniModelDTO> Update(MiniModelDTO miniModel, int id);
         // DeleteAMiniModel
+        // DeleteAMiniModel
+        /// <summary>
+        /// the below method deletes a minimodel item from the database
+        /// </summary>
+        /// <param name="supplyId">the minimodel item to be deleted</param>
+        /// <returns>nothing, the item is deleted</returns>
         Task Delete(int id);
 
         // add a paint to a model
